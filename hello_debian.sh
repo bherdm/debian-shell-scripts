a90b939f66c71894ca58df68235adef26fa20b56
Bradley Herdman <bherdm@gmail.com>
2025-01-09 18:09:41 -0600

    Initial MakeMkv build attempt

--- hello_debian.sh
+++ hello_debian.sh
@@ -58,6 +58,85 @@ flatpak install -y https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
 
 #Blender
 
+#MAKEMKV
+sudo apt-get update -qq && sudo apt-get -y install \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  git-core \
+  libass-dev \
+  libfreetype6-dev \
+  libgnutls28-dev \
+  libmp3lame-dev \
+  libsdl2-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  meson \
+  ninja-build \
+  pkg-config \
+  texinfo \
+  wget \
+  yasm \
+  zlib1g-dev
+#nasm
+cd ~/ffmpeg_sources && \
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.bz2 && \
+tar xjvf nasm-2.16.01.tar.bz2 && \
+cd nasm-2.16.01 && \
+./autogen.sh && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
+make && \
+make install
+#libfdk_aac
+cd ~/ffmpeg_sources && \
+git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
+cd fdk-aac && \
+autoreconf -fiv && \
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
+make && \
+make install
+# #ffmpeg
+# rm -r ~/ffmpeg_sources
+# mkdir ~/ffmpeg_sources
+# cd ~/ffmpeg_sources && \
+# wget -O ffmpeg-7.1.tar.bz2 https://ffmpeg.org//releases/ffmpeg-7.1.tar.bz2 && \
+# tar xjvf ffmpeg-7.1.tar.bz2 && \
+# cd ffmpeg && \
+# PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+#   --prefix="$HOME/ffmpeg_build" \
+#   --pkg-config-flags="--static" \
+#   --extra-cflags="-I$HOME/ffmpeg_build/include" \
+#   --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+#   --extra-libs="-lpthread -lm" \
+#   --ld="g++" \
+#   --bindir="$HOME/bin" \
+#   --enable-gpl \
+#   --enable-gnutls \
+#   --enable-libaom \
+#   --enable-libass \
+#   --enable-libfdk-aac \
+#   --enable-libfreetype \
+#   --enable-libmp3lame \
+#   --enable-libopus \
+#   --enable-libsvtav1 \
+#   --enable-libdav1d \
+#   --enable-libvorbis \
+#   --enable-libvpx \
+#   --enable-libx264 \
+#   --enable-libx265 \
+#   --enable-nonfree && \
+# PATH="$HOME/bin:$PATH" make && \
+# make install && \
+# hash -r
+
+
+
 mkdir ~/git
 #bherdm/Debian-Shell-Scripts
 cd ~/git
