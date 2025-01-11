4c287a19775bb987cec48e1cb2e5cd0037736c08
bherdm <146450154+bherdm@users.noreply.github.com>
2025-01-11 13:01:14 -0600

    Changed Firefox/Ublock install to end of hello script

--- hello_debian.sh
+++ hello_debian.sh
@@ -11,32 +11,6 @@ sudo apt-get install -y \
   espeak \
   vlc
 
-#FIREFOX
-#Create a directory to store APT repository keys if it doesn't exist:
-sudo install -d -m 0755 /etc/apt/keyrings
-
-#Import the Mozilla APT repository signing key:
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-#If you do not have wget installed, you can install it with:
-sudo apt-get install -y \
-  wget
-#The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command:
-gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-#Next, add the Mozilla APT repository to your sources list:
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-#Configure APT to prioritize packages from the Mozilla repository:
-echo '
-Package: *
-Pin: origin packages.mozilla.org
-Pin-Priority: 1000
-' | sudo tee /etc/apt/preferences.d/mozilla
-#Update your package list, and install the Firefox .deb package:
-sudo apt-get update && sudo apt-get install -y firefox
-
-#UBLOCK ORIGIN
-sudo apt-get install -y python3
-python3 -m webbrowser -t  https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/ &
-
 #VISUAL STUDIO CODE
 sudo rm /etc/apt/preferences.d/nosnap.pref #Needed for Mint
 sudo apt-get install -y snapd
@@ -80,5 +54,32 @@ git clone https://github.com/bherdm/debian-shell-scripts.git
 #xdg-open .
 bash ~/git/debian-shell-scripts/godot-for-ouya-setup.sh
 bash ~/git/debian-shell-scripts/benchmark_dependencies.sh
+
+#FIREFOX
+#Create a directory to store APT repository keys if it doesn't exist:
+sudo install -d -m 0755 /etc/apt/keyrings
+
+#Import the Mozilla APT repository signing key:
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+#If you do not have wget installed, you can install it with:
+sudo apt-get install -y \
+  wget
+#The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command:
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+#Next, add the Mozilla APT repository to your sources list:
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+#Configure APT to prioritize packages from the Mozilla repository:
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+#Update your package list, and install the Firefox .deb package:
+sudo apt-get update && sudo apt-get install -y firefox
+
+#UBLOCK ORIGIN
+sudo apt-get install -y python3
+python3 -m webbrowser -t  https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/ &
+
 # Wait
 read -p "Finished! Press Enter to exit..."
